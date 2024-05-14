a7fe272d1f2c0f336c67c0ed171c17110158c504
rene-belin <rene.b@gmx.fr>
2024-05-14 14:36:19 +0200

    advertPage finaliser

--- advert-page.php
+++ advert-page.php
@@ -6,7 +6,19 @@ require_once("components/navbar.php");
 
 <div class="container">
     <div class="row">
-        <h1>Mon titre</h1> 
+        <div class="col-md-6">
+            <img src="https://picsum.photos/800" alt="">
+        </div>
+        <div class="col-md-6">
+            <h1>Mon titre</h1>
+            <p>Thomas - Bordeaux - Le 01/10/25 à 22:00</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque sit dolorem, neque quam laboriosam ad distinctio at quibusdam et tenetur possimus harum facere totam? Dicta nisi provident sit quae a!
+            Dolorum, ut amet in voluptas consectetur assumenda iure distinctio enim omnis quos ullam, quibusdam molestias obcaecati odio, impedit cupiditate nobis repudiandae reiciendis. Optio repudiandae recusandae deleniti commodi, eaque blanditiis? Praesentium?
+            Dolore et consequatur ex doloribus quasi nihil quae animi, blanditiis fugit quia illum reprehenderit nulla enim nesciunt suscipit pariatur eius, ad similique tempora. Repellendus sint recusandae vitae nam cumque dolorum?
+            Quis incidunt numquam porro pariatur libero cupiditate, a eligendi culpa ducimus nulla tenetur. Repellendus, suscipit ducimus voluptatibus doloribus minus tempora nobis enim at ad? Similique, nisi rerum? Aut, ex sequi?
+            Cum facilis nesciunt perspiciatis ducimus vitae impedit dolore ea, cupiditate tenetur nobis quibusdam, dolorum doloremque voluptatibus quidem obcaecati accusantium soluta corrupti? Voluptatum animi ea eligendi itaque soluta adipisci beatae enim.
+            Officia maiores reiciendis recusandae omnis earum, laudantium voluptates consequatur rem. Animi quisquam et magnam debitis nostrum officiis libero nisi rem explicabo, fuga minima accusantium, accusamus, omnis deleniti quam incidunt dolor.</p>
+        </div>
     </div>
 </div>
 
@@ -14,4 +26,3 @@ require_once("components/navbar.php");
 <?php
 require_once("components/footer.php");
 ?>
-
